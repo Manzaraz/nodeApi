3081d8c2152db5764c67741105fa25d9493e2abe
manzaraz <magnusmanz@gmaild.com>
2022-07-20 16:55:12 -0300

    logger configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import ip from "ip";
 import dotenv from "dotenv";
 import cors from "cors";
+import Response from "./domain/response.js";
+import logger from "./utils/logger.js";
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -11,11 +13,9 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ message: "UP" });
+  res.send(new Response(200, "OK", "Patient API , v1.0.0 - All Systems Go!"));
 });
 
-console.log(process.env);
-
 app.listen(PORT, () => {
-  console.log(`Server is running on: ${ip.address()}:${PORT}`);
+  logger.info(`Server is running on: ${ip.address()}:${PORT}`);
 });
